74039314cd3813967c38cf676ed96235101dff56
Dan Moore <dan@fusionauth.io>
2026-07-24 10:48:13 -0600

    changed location of fusionauth server

--- protected-remote-mcp/start.sh
+++ protected-remote-mcp/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 MCP_SERVER_URL=http://localhost:8000 \
-FUSIONAUTH_URL=https://pre-feature-resource-indicator-rfc8707.dev.us-west-2.fusionauth.co \
-FUSIONAUTH_EXTERNAL_URL=https://pre-feature-resource-indicator-rfc8707.dev.us-west-2.fusionauth.co \
+FUSIONAUTH_URL=https://sandbox.fusionauth.io \
+FUSIONAUTH_EXTERNAL_URL=https://sandbox.fusionauth.io \
 docker compose up -d
